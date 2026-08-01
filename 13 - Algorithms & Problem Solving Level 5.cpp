65fd143ab0b008717ac85c1f9c7cad2745a858f6
Yahia Alsebaie <YahiaAlsebaie@gmail.com>
2026-08-01 16:33:13 +0300

    Project 6 - Add `clsMyStackArr` and MyStackClassArrTest.cpp

--- 13 - Algorithms & Problem Solving Level 5.cpp
+++ 13 - Algorithms & Problem Solving Level 5.cpp
@@ -1,71 +1,67 @@
 
-#include "clsMyQueueArr.h"
-#include <cstdlib>
 #include <iostream>
+#include "clsMyStackArr.h"
 
 using namespace std;
 
 int main()
 {
 
-	clsMyQueueArr <int> MyQueue;
+    clsMyStackArr <int> MyStack;
 
-	MyQueue.push(10);
-	MyQueue.push(20);
-	MyQueue.push(30);
-	MyQueue.push(40);
-	MyQueue.push(50);
-
-
-	cout << "\nQueue: \n";
-	MyQueue.Print();
-
-	cout << "\nQueue Size: " << MyQueue.Size();
-	cout << "\nQueue Front: " << MyQueue.front();
-	cout << "\nQueue Back: " << MyQueue.back();
-
-	MyQueue.pop();
-
-	cout << "\n\nQueue after pop() : \n";
-	MyQueue.Print();
-
-	//Extension #1
-	cout << "\n\n Item(2) : " << MyQueue.GetItem(2);
-
-	//Extension #2
-	MyQueue.Reverse();
-	cout << "\n\nQueue after reverse() : \n";
-	MyQueue.Print();
-
-	//Extension #3
-	MyQueue.UpdateItem(2, 600);
-	cout << "\n\nQueue after updating Item(2) to 600 : \n";
-	MyQueue.Print();
-
-	//Extension #4
-	MyQueue.InsertAfter(2, 800);
-	cout << "\n\nQueue after Inserting 800 after Item(2) : \n";
-	MyQueue.Print();
+    MyStack.push(10);
+    MyStack.push(20);
+    MyStack.push(30);
+    MyStack.push(40);
+    MyStack.push(50);
 
 
-	//Extension #5
-	MyQueue.InsertAtFront(1000);
-	cout << "\n\nQueue after Inserting 1000 at front: \n";
-	MyQueue.Print();
+    cout << "\nStack: \n";
+    MyStack.Print();
 
-	//Extension #6
-	MyQueue.InsertAtBack(2000);
-	cout << "\n\nQueue after Inserting 2000 at back: \n";
-	MyQueue.Print();
+    cout << "\nStack Size: " << MyStack.Size();
+    cout << "\nStack Top: " << MyStack.Top();
+    cout << "\nStack Bottom: " << MyStack.Bottom();
 
-	//Extension #7
-	MyQueue.Clear();
-	cout << "\n\nQueue after Clear(): \n";
-	MyQueue.Print();
+    MyStack.pop();
+
+    cout << "\n\nStack after pop() : \n";
+    MyStack.Print();
+
+    //Extension #1
+    cout << "\n\n Item(2) : " << MyStack.GetItem(2);
+
+    //Extension #2
+    MyStack.Reverse();
+    cout << "\n\nStack after reverse() : \n";
+    MyStack.Print();
+
+    //Extension #3
+    MyStack.UpdateItem(2, 600);
+    cout << "\n\nStack after updating Item(2) to 600 : \n";
+    MyStack.Print();
+
+    //Extension #4
+    MyStack.InsertAfter(2, 800);
+    cout << "\n\nStack after Inserting 800 after Item(2) : \n";
+    MyStack.Print();
 
 
+    //Extension #5
+    MyStack.InsertAtFront(1000);
+    cout << "\n\nStack after Inserting 1000 at top: \n";
+    MyStack.Print();
 
+    //Extension #6
+    MyStack.InsertAtBack(2000);
+    cout << "\n\nStack after Inserting 2000 at bottom: \n";
+    MyStack.Print();
 
-	system("pause>0");
+    //Extension #7
+    MyStack.Clear();
+    cout << "\n\nStack after Clear(): \n";
+    MyStack.Print();
+
+    system("pause>0");
 
 }
---
+++ clsMyStackArr.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "clsMyQueueArr.h"
+
+using namespace std;
+
+template <class T>
+class clsMyStackArr : public clsMyQueueArr <T>
+{
+
+public:
+
+	void push(T item)
+	{
+		clsMyQueueArr<T>::InsertAtFront(item);
+	}
+
+
+	T Top()
+	{
+		return clsMyQueueArr<T>::front();
+	}
+
+	T Bottom()
+	{
+		return clsMyQueueArr<T>::back();
+	}
+
+};
+
+/* // Composition Way
+template <class T>
+class clsMyStackArr
+{
+private:
+	clsDblLinkedList<T> _myList; // Or clsMyQueueArr<T> _myQueue;
+
+public:
+	void push(T item)  { _myList.InsertAtBeginning(item); }
+	void pop()         { _myList.DeleteFirstNode(); }
+	T Top()            { _myList.GetHeadValue(); }
+	T Bottom()         { _myList.GetTailValue(); }
+	bool IsEmpty()     { return _myList.IsEmpty(); }
+	size_t Size()      { return _myList.Size(); }
+};
+
+*/
+
+
+
