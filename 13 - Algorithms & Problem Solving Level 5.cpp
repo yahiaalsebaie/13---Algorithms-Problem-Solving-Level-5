724a9f92f3968c596f25be494cbe1021676f1050
Yahia Alsebaie <YahiaAlsebaie@gmail.com>
2026-08-01 12:30:37 +0300

    Project 4 : clsDynamicArray
    Added `clsDynamicArray.h`, which defines a template class for dynamic arrays with features like dynamic resizing, in-place reversal, and memory management.

--- 13 - Algorithms & Problem Solving Level 5.cpp
+++ 13 - Algorithms & Problem Solving Level 5.cpp
@@ -1,67 +1,37 @@
 
-#include "clsMyStack.h"
+
 #include <iostream>
+#include "clsDynamicArray.h"
 
 using namespace std;
 
 int main()
 {
 
-	clsMyStack <int> MyStack;
+    clsDynamicArray <int> MyDynamicArray(5);
 
-	MyStack.push(10);
-	MyStack.push(20);
-	MyStack.push(30);
-	MyStack.push(40);
-	MyStack.push(50);
+    MyDynamicArray.SetItem(0, 10);
+    MyDynamicArray.SetItem(1, 20);
+    MyDynamicArray.SetItem(2, 30);
+    MyDynamicArray.SetItem(3, 40);
+    MyDynamicArray.SetItem(4, 50);
 
+    cout << "\nIs Empty?  " << MyDynamicArray.IsEmpty();
+    cout << "\nArray Size: " << MyDynamicArray.Size() << "\n";
+    cout << "\nArray Items: \n";
 
-	cout << "\nStack: \n";
-	MyStack.Print();
+    MyDynamicArray.PrintList();
 
-	cout << "\nStack Size: " << MyStack.Size();
-	cout << "\nStack Top: " << MyStack.Top();
-	cout << "\nStack Bottom: " << MyStack.Bottom();
+    MyDynamicArray.DeleteFirstItem();
+    cout << "\nArray Items after deleting FirstItem: \n";
+    cout << "\nArray Size: " << MyDynamicArray.Size() << "\n";
+    MyDynamicArray.PrintList();
 
-	MyStack.pop();
+    MyDynamicArray.DeleteLastItem();
+    cout << "\nArray Items after deleting LastItem: \n";
+    cout << "\nArray Size: " << MyDynamicArray.Size() << "\n";
+    MyDynamicArray.PrintList();
 
-	cout << "\n\nStack after pop() : \n";
-	MyStack.Print();
-
-	//Extension #1
-	cout << "\n\n Item(2) : " << MyStack.GetItem(2);
-
-	//Extension #2
-	MyStack.Reverse();
-	cout << "\n\nStack after reverse() : \n";
-	MyStack.Print();
-
-	//Extension #3
-	MyStack.UpdateItem(2, 600);
-	cout << "\n\nStack after updating Item(2) to 600 : \n";
-	MyStack.Print();
-
-	//Extension #4
-	MyStack.InsertAfter(2, 800);
-	cout << "\n\nStack after Inserting 800 after Item(2) : \n";
-	MyStack.Print();
-
-
-	//Extension #5
-	MyStack.InsertAtFront(1000);
-	cout << "\n\nStack after Inserting 1000 at top: \n";
-	MyStack.Print();
-
-	//Extension #6
-	MyStack.InsertAtBack(2000);
-	cout << "\n\nStack after Inserting 2000 at bottom: \n";
-	MyStack.Print();
-
-	//Extension #7
-	MyStack.Clear();
-	cout << "\n\nStack after Clear(): \n";
-	MyStack.Print();
-
-	system("pause>0");
+    system("pause>0");
 
 }
---
+++ clsDynamicArray.h
@@ -0,0 +1,140 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <utility>
+
+using namespace std;
+
+template <class T>
+class clsDynamicArray
+{
+protected:
+	size_t _size = 0;
+	T* _OriginalArray;
+	T* _TempArray;
+
+	void _Swap(T& item1, T& item2)
+	{
+		T temp = item1;
+		item1 = item2;
+		item2 = temp;
+	}
+public:
+
+	clsDynamicArray(size_t size = 0)
+	{
+		if (size < 0) size = 0;
+
+		_size = size;
+
+		_OriginalArray = new T[_size];
+	}
+
+	~clsDynamicArray()
+	{
+		Clear();
+	}
+
+	bool SetItem(size_t index, T value)
+	{
+		if (index >= _size || _size < 0)
+			return false;
+
+		_OriginalArray[index] = value;
+		return true;
+	}
+
+	bool IsEmpty()
+	{
+		return (_size == 0);
+	}
+
+	size_t Size()
+	{
+		return _size;
+	}
+
+	void PrintList(std::string Separator = " ")
+	{
+		for (size_t i = 0; i < _size; i++)
+		{
+			std::cout << _OriginalArray[i] << Separator;
+		}
+		std::cout << std::endl;
+	}
+
+	void Resize(size_t newSize)
+	{
+		if (newSize < 0) newSize = 0;
+
+		_TempArray = new T[newSize];
+
+		if (newSize < _size)
+			_size = newSize;
+
+		for (size_t i = 0; i < _size; i++)
+		{
+			_TempArray[i] = _OriginalArray[i];
+		}
+
+		_size = newSize;
+
+		delete[] _OriginalArray;
+		_OriginalArray = _TempArray;
+	}
+
+	T GetItem(size_t index)
+	{
+		return (index >= _size || _OriginalArray == nullptr) ? T() : _OriginalArray[index];
+	}
+
+
+
+	void Reverse()
+	{
+		if (_size <= 1 || _OriginalArray == nullptr) return;
+
+		/*_TempArray = new T[_size];
+		size_t counter = 0;
+
+		for (size_t i = 0; i < _size; i++)
+		{
+			_TempArray[i] = _OriginalArray[_size - 1 - i];
+		}
+		delete[] _OriginalArray;
+		_OriginalArray = _TempArray;*/
+
+		//O(n/2)
+		for (size_t i = 0; i < _size / 2; i++)
+		{
+			_Swap(_OriginalArray[i], _OriginalArray[_size - 1 - i]);
+		}
+	}
+
+	void Clear()
+	{
+		delete[] _OriginalArray;
+		_OriginalArray = nullptr;
+		_size = 0;
+	}
+
+	bool DeleteItemAt(size_t index)
+	{
+		if (_OriginalArray == nullptr || index >= _size) return false;
+		if (_size <= 1)
+		{
+			Clear();
+			return true;
+		}
+		for (size_t i = index; i < _size - 1; i++)
+		{
+			_OriginalArray[i] = _OriginalArray[i + 1];
+		}
+		_size--;
+
+		return true;
+	}
+
+
+};
+
