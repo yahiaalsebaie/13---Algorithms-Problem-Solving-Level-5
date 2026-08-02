8de426232100c39044306bee04a5302992b42bee
Yahia Alsebaie <YahiaAlsebaie@gmail.com>
2026-08-02 21:17:13 +0300

    Project 8: clsQueueLine v1

--- 13 - Algorithms & Problem Solving Level 5.cpp
+++ 13 - Algorithms & Problem Solving Level 5.cpp
@@ -1,54 +1,56 @@
-#include <iostream>
 #include "clsQueueLine.h"
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
 
-    clsQueueLine PayBillsQueue("A0", 10);
-    clsQueueLine SubscriptionsQueue("B0", 5);
+	clsQueueLine PayBillsQueue("A0", 10);
+	clsQueueLine SubscriptionsQueue("B0", 5);
 
 
-    PayBillsQueue.IssueTicket();
-    PayBillsQueue.IssueTicket();
-    PayBillsQueue.IssueTicket();
-    PayBillsQueue.IssueTicket();
-    PayBillsQueue.IssueTicket();
+	PayBillsQueue.IssueTicket();
+	PayBillsQueue.IssueTicket();
+	PayBillsQueue.IssueTicket();
+	PayBillsQueue.IssueTicket();
+	PayBillsQueue.IssueTicket();
 
-    cout << "\nPay Bills Queue Info:\n";
-    PayBillsQueue.PrintInfo();
+	cout << "\nPay Bills Queue Info:\n";
+	PayBillsQueue.PrintInfo();
 
-    PayBillsQueue.PrintTicketsLineRTL();
-    PayBillsQueue.PrintTicketsLineLTR();
-
-
-
-    PayBillsQueue.PrintAllTickets();
-
-    PayBillsQueue.ServeNextClient();
-    cout << "\nPay Bills Queue After Serving One client\n";
-    PayBillsQueue.PrintInfo();
-
-    cout << "\nSubscriptions Queue Info:\n";
-
-    SubscriptionsQueue.IssueTicket();
-    SubscriptionsQueue.IssueTicket();
-    SubscriptionsQueue.IssueTicket();
+	PayBillsQueue.PrintTicketsLineRTL();
+	PayBillsQueue.PrintTicketsLineLTR();
 
 
-    SubscriptionsQueue.PrintInfo();
-
-    SubscriptionsQueue.PrintTicketsLineRTL();
-    SubscriptionsQueue.PrintTicketsLineLTR();
-
-    SubscriptionsQueue.PrintAllTickets();
-
-    SubscriptionsQueue.ServeNextClient();
-    cout << "\nSubscriptions Queue After Serving One client\n";
-    SubscriptionsQueue.PrintInfo();
+	PayBillsQueue.PrintAllTickets();
 
 
-    return 0;
+	PayBillsQueue.ServeNextClient();
+	cout << "\nPay Bills Queue After Serving One client\n";
+	PayBillsQueue.PrintInfo();
+
+	cout << "\nSubscriptions Queue Info:\n";
+
+	SubscriptionsQueue.IssueTicket();
+	SubscriptionsQueue.IssueTicket();
+	SubscriptionsQueue.IssueTicket();
+
+
+	SubscriptionsQueue.PrintInfo();
+
+	SubscriptionsQueue.PrintTicketsLineRTL();
+	SubscriptionsQueue.PrintTicketsLineLTR();
+
+	SubscriptionsQueue.PrintAllTickets();
+
+	SubscriptionsQueue.ServeNextClient();
+	cout << "\nSubscriptions Queue After Serving One client\n";
+	SubscriptionsQueue.PrintInfo();
+
+	int a;
+	cin >> a;
+
+	return 0;
 }
 
--- clsQueueLine.h
+++ clsQueueLine.h
@@ -1,47 +1,121 @@
 #pragma once
+#include "clsDate.h"
+#include "clsMyQueue.h"
+#include "clsUtil.h"
+#include <iostream>
 #include <string>
 
 using namespace std;
+
 class clsQueueLine
 {
 private:
+	std::string _prefix = "";
+	int _operationTime = 0;
+	clsMyQueue<int> _queue;
+	int _servedClients = 0;
+	int _totalTickets = 0;
 
-public:
-	//clsQueueLine() = default;
 
-	clsQueueLine(const std::string& prefix, int Num )
+private:
+	int GetWaitingClients() const
 	{
-
+		return _totalTickets - _servedClients;
+	}
+	static string Spaces(short count = 37)
+	{
+		return std::string(count, ' ');
 	}
 
+public:
 
+	/*clsQueueLine(const std::string& prefix, int OpTime)
+	{
+		_prefix = prefix;
+		_operationTime = OpTime;
+	}*/
+	clsQueueLine(const std::string& prefix, const int& OperationTime) : _prefix(prefix), _operationTime(OperationTime) {}
+
+	
 	void IssueTicket()
 	{
+		++_totalTickets;
+		_queue.push(_totalTickets);
 
 	}
 
 	void PrintInfo()
 	{
 
+		string separatorLine = clsUtil::ColorText("===========================================\n", clsUtil::enColor::BRIGHT_CYAN);
+
+		cout << Spaces() << separatorLine;
+		cout << Spaces() << "\t\t\tQueue Info\n";
+		cout << Spaces() << separatorLine;
+		cout << Spaces() << "\t Prefix          = " << _prefix << "\n";
+		cout << Spaces() << "\t Total Tickets   = " << _totalTickets << "\n";
+		cout << Spaces() << "\t Served Clients  = " << _servedClients << "\n";
+		cout << Spaces() << "\t Waiting Clients = " << GetWaitingClients() << "\n";
+		cout << Spaces() << separatorLine << endl;
+
 	}
 
-	void PrintTicketsLineRTL()
+	void PrintTicketsLineRTL() const
 	{
+		string rArrow = clsUtil::ColorText(" <-- ", clsUtil::enColor::BRIGHT_GREEN);
 
+		cout << Spaces(30) << " Tickets: ";
+		for (int i = 1; i <= _totalTickets; i++)
+		{
+			cout << _prefix << i << rArrow;
+		}
+		cout << endl;
 	}
 
-	void PrintTicketsLineLTR()
+	void PrintTicketsLineLTR() const
 	{
+		string rArrow = clsUtil::ColorText(" --> ", clsUtil::enColor::BRIGHT_GREEN);
 
+		cout << Spaces(30) << " Tickets: ";
+		for (int i = _totalTickets; i >= 1; i--)
+		{
+			cout << _prefix << i << rArrow;
+		}
+		cout << endl;
+	}
+
+	void PrintTicket(int ticketNumber) const
+	{
+		string separatorLine = clsUtil::ColorText("_______________________________________\n", clsUtil::enColor::BRIGHT_MAGENTA);
+
+		cout << Spaces(40) << separatorLine;
+		cout << Spaces(40) << "\t\t  " << _prefix << ticketNumber << "\n";
+		cout << Spaces(40) << "\t " << clsDate::GetSystemDateTimeString() << endl;
+		cout << Spaces(40) << "\t Waiting Clients = " << (--ticketNumber) << "\n";
+		cout << Spaces(40) << "\t Serve Time In " << (_operationTime * (ticketNumber)) << " Minutes.\n";
+		cout << Spaces(40) << separatorLine;
+		cout << endl;
 	}
 
 	void PrintAllTickets()
 	{
+		cout << endl; 
+		string Title = clsUtil::ColorText("\t\t     ---Tickets---\n",clsUtil::enColor::BRIGHT_GREEN);
+
+		cout << Spaces(35) << Title;
+		for (int ticketNumber = 1; ticketNumber <= GetWaitingClients(); ++ticketNumber)
+		{
+			PrintTicket(ticketNumber);
+		}
 
 	}
 
 	void ServeNextClient()
 	{
+		if (_queue.IsEmpty()) return;
+		
+		_queue.pop();
+		++_servedClients;
 
 	}
 
