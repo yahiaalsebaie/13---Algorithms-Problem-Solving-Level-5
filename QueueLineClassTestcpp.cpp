d545e6ecf68264ad8c0b91c1b007570cb150f155
Yahia Alsebaie <YahiaAlsebaie@gmail.com>
2026-08-03 15:38:59 +0300

    Add QueueLineClassTest.cpp

---
+++ QueueLineClassTestcpp.cpp
@@ -0,0 +1,58 @@
+#include "clsQueueLine.h"
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	clsQueueLine PayBillsQueue("A0", 10);
+	clsQueueLine SubscriptionsQueue("B0", 5);
+
+
+	PayBillsQueue.IssueTicket();
+	PayBillsQueue.IssueTicket();
+	PayBillsQueue.IssueTicket();
+	PayBillsQueue.IssueTicket();
+	PayBillsQueue.IssueTicket();
+
+	cout << "\nPay Bills Queue Info:\n";
+	PayBillsQueue.PrintInfo();
+
+	PayBillsQueue.PrintTicketsLineRTL();
+	PayBillsQueue.PrintTicketsLineLTR();
+
+
+	PayBillsQueue.PrintAllTickets();
+
+
+	PayBillsQueue.ServeNextClient();
+	cout << "\nPay Bills Queue After Serving One client\n";
+	PayBillsQueue.PrintInfo();
+
+	cout << "\nSubscriptions Queue Info:\n";
+
+	SubscriptionsQueue.IssueTicket();
+	SubscriptionsQueue.IssueTicket();
+	SubscriptionsQueue.IssueTicket();
+
+
+	SubscriptionsQueue.PrintInfo();
+
+	SubscriptionsQueue.PrintTicketsLineRTL();
+	SubscriptionsQueue.PrintTicketsLineLTR();
+
+	SubscriptionsQueue.PrintAllTickets();
+
+	SubscriptionsQueue.ServeNextClient();
+	cout << "\nSubscriptions Queue After Serving One client\n";
+	SubscriptionsQueue.PrintInfo();
+
+
+	cout << "\nNext Clients Is: [" << SubscriptionsQueue.WhoIsNext() << "]\n" << endl;
+
+	system("pause>0");
+	return 0;
+}
+
